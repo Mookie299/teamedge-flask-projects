e2ebd5743543e4366cb866b20d97cf31427759e1
Mookie299 <Daniellabillini@gmail.com>
2021-03-27 13:22:38 -0400

    templates

--- forms/app.py
+++ forms/app.py
@@ -5,17 +5,16 @@ from time import sleep
 sense = SenseHat ()
 
 app = Flask(__name__)
-
+#yerrrrrr
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/sucess',methods=['GET','POST'])
+@app.route('/success',methods=['POST'])
 def sucess():
-    message = request.form['message'] 
-    return render_template("sucess.html",message = message)
+    message = request.form['message']
     sense.show_message(message)
-
+    return render_template("success.html",message = message)
     
 if __name__ == '__main__':
     app.run(debug = True, host ='0.0.0.0')
